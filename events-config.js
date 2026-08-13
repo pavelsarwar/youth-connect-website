6955e361cc42bb8d8a5852d129aad5a957df4347
Pavel Sarwar <pavelsarwar@users.noreply.github.com>
2026-08-13 21:09:57 +0800

    Update events-config.js

--- events-config.js
+++ events-config.js
@@ -2,7 +2,7 @@
 // Paste the shareable Google Sheet URL below after creating the sheet.
 // The sheet must be viewable by "Anyone with the link".
 window.YCF_EVENTS_CONFIG = {
-  sheetUrl: "",
+  sheetUrl: "https://docs.google.com/spreadsheets/d/1SGCcO7UvghLDgCbILqHxYDpMOH_cDelA4eTWhCx-3Dg/edit?usp=sharing",
   maxEvents: 4,
   showPastEvents: false
 };
